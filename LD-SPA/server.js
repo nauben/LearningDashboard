139e0b65bbd414de9bbcd8f0114a2ecac05eb441
nauben <40056731+nauben@users.noreply.github.com>
2020-10-12 18:12:18 +0200

    Update server.js

--- LD-SPA/server.js
+++ LD-SPA/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/LD-SPA'));
+ngApp.use(express.static('./dist/ld-spa'));
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/LD-SPA/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/ld-spa/index.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
