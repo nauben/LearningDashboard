abdf654a781f4c730427622e9d260f515e79b505
nauben <40056731+nauben@users.noreply.github.com>
2020-10-22 00:51:38 +0200

    Updated ehcache config

---
+++ LD-API/src/main/java/com/mosbach/ld/config/EhCacheConfigurer.java
@@ -0,0 +1,27 @@
+package com.mosbach.ld.config;
+
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@EnableCaching
+public class EhCacheConfigurer {
+
+	@Bean
+	public CacheManager cacheManager() {
+		return new EhCacheCacheManager(cacheManagerFactory().getObject());
+	}
+	
+	public EhCacheManagerFactoryBean cacheManagerFactory() {
+		EhCacheManagerFactoryBean bean = new EhCacheManagerFactoryBean();
+		bean.setConfigLocation(new ClassPathResource("ehcache.xml"));
+		bean.setShared(true);
+		return bean;
+	}
+}
