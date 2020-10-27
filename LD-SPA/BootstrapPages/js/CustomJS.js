c60cfd8a350cd9bd88f10a9b64baef8e2f72a26f
Code-TH <71486844+Code-TH@users.noreply.github.com>
2020-10-27 22:42:02 +0100

    Responsive Start Page
    dropdown menu on smaller screens to select Kanban-swimlanes

--- LD-SPA/BootstrapPages/js/CustomJS.js
+++ LD-SPA/BootstrapPages/js/CustomJS.js
@@ -1,3 +1,4 @@
+/*eslint-env es6*/
 function showPassword1L() {
 		var x = document.getElementById("Password-Login");
 		if (x.type === "password") {
@@ -22,6 +23,7 @@ function showPassword2L() {
 				}
 }
 
+//####################
 //Password Register validation
 function validatePassword(){
 var myInput = document.getElementById("Password-Register");
@@ -82,3 +84,160 @@ myInput.onkeyup = function() {
   }
 }
 }
+//####################
+
+//####################
+//Enable drag and drop for Kanban-Board 
+
+	const drag = (event) => {
+	event.dataTransfer.setData("text/plain", event.target.id);
+	}
+
+	const allowDrop = (ev) => {
+	ev.preventDefault();
+	if (hasClass(ev.target,"dropzone")) {
+		addClass(ev.target,"droppable");
+	}
+	}
+
+	const clearDrop = (ev) => {
+		removeClass(ev.target,"droppable");
+	}
+
+	const drop = (event) => {
+	event.preventDefault();
+	const data = event.dataTransfer.getData("text/plain");
+	const element = document.querySelector(`#${data}`);
+	try {
+		// remove the spacer content from dropzone
+		event.target.removeChild(event.target.firstChild);
+		// add the draggable content
+		event.target.appendChild(element);
+		// remove the dropzone parent
+		unwrap(event.target);
+	} catch (error) {
+	return null;
+	}
+	updateDropzones();
+	}
+
+	const updateDropzones = () => { 
+		/* after dropping, refresh the drop target areas
+		  so there is a dropzone after each item and after the heading
+		  using jQuery here for simplicity */
+
+		var dz = '<div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"> &nbsp; </div>';
+
+		// delete old dropzones
+		$('.dropzone').remove();
+		
+		/*// insert new dropzone after swimlane name (custom)
+		dz.insertAfter('.card-subtitle.draggable'); */
+		
+		// insert new dropzone after each item (deprecated)
+		//dz.insertAfter('.card.draggable');
+	
+		
+		$('.items').prepend(dz);
+		$('.card.draggable').after(dz);
+		
+		// insert new dropzone in any empty swimlanes (deprecated)
+		//$(".items:not(:has(.card.draggable))").append(dz);
+		
+		
+	};
+
+	// helpers
+	function hasClass(target, className) {
+		return new RegExp('(\\s|^)' + className + '(\\s|$)').test(target.className);
+	}
+
+	function addClass(ele,cls) {
+	if (!hasClass(ele,cls)) ele.className += " "+cls;
+	}
+
+	function removeClass(ele,cls) {
+	if (hasClass(ele,cls)) {
+		var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+		ele.className=ele.className.replace(reg,' ');
+	}
+	}
+
+	function unwrap(node) {
+		node.replaceWith(...node.childNodes);
+	}
+//####################
+function chooseSwimlane(){
+var sl = $("#swimlane").val();
+var todo = $("#todo")[0];
+var inarbeit = $("#inarbeit")[0];
+var fertig = $("#fertig")[0];
+
+	
+	if	(sl === "In Arbeit"){
+		todo.setAttribute('style', 'display:none !important');
+		inarbeit.setAttribute('style', 'display:block !important');
+		fertig.setAttribute('style', 'display:none !important');
+	}else if(sl === "Fertig"){ 
+		todo.setAttribute('style', 'display:none !important');
+		inarbeit.setAttribute('style', 'display:none !important');
+		fertig.setAttribute('style', 'display:block !important');
+	}else {
+		todo.setAttribute('style', 'display:block !important');
+		inarbeit.setAttribute('style', 'display:none !important');
+		fertig.setAttribute('style', 'display:none !important');
+	}$('#swimlane').val("Swimlane auswählen");
+} 
+
+//notice the particular screen size
+function sizeKan(){
+	let envs = ['xs', 'sm', 'md', 'lg', 'xl'];
+
+    let el = document.createElement('div');
+    document.body.appendChild(el);
+
+    let curEnv = envs.shift();
+
+    for (let env of envs.reverse()) {
+        el.classList.add(`d-${env}-none`);
+
+        if (window.getComputedStyle(el).display === 'none') {
+            curEnv = env;
+            break;
+        }
+    }
+
+    document.body.removeChild(el);
+    resizeKan(curEnv);
+	
+}
+
+//if scren size is xl or lg, then show all three swimlanes of the Kanban-Board
+function resizeKan(curEnv){
+	var todo = $("#todo")[0];
+	var inarbeit = $("#inarbeit")[0];
+	var fertig = $("#fertig")[0];
+	var size = curEnv;
+
+	if(size === "lg" || size === "xl"){
+		todo.style.display = "block";
+		inarbeit.style.display = "block";
+		fertig.style.display = "block";	
+	}
+	
+	/*if(size === "xs" || size === "sm" || size === "md"){
+		todo.style.display = "block";
+		inarbeit.style.display = "none";
+		fertig.style.display = "none";	
+	} */
+	
+	
+}
+
+
+//hide/show sidebar
+$(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
