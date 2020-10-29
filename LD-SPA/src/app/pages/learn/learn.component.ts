b2e0a228ee44f647435577f32ca4579f59d5e95a
Code-TH <71486844+Code-TH@users.noreply.github.com>
2020-10-30 00:08:48 +0100

    Intermediate status SPA
    commit due to settings change (push directly from VS Code)

--- LD-SPA/src/app/pages/learn/learn.component.ts
+++ LD-SPA/src/app/pages/learn/learn.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker'; 
+import {Chart} from 'node_modules/chart.js'
 
 @Component({
   selector: 'app-learn',
@@ -10,6 +12,37 @@ export class LearnComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var ctx = document.getElementById('PieChart');
+    var PieChart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: {
+            labels: ['VWL', 'Statistik', 'Agiles Projektmanagement'],
+            datasets: [{
+                label: 'minutes',
+                data: [205, 295, 224],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
   }
 
 }
