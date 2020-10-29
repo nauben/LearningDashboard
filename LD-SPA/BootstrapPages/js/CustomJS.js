c72aa0d7cdf425eb60e2cd7470584f362ffda941
lukas7777777 <71486756+lukas7777777@users.noreply.github.com>
2020-10-29 19:36:56 +0100

    Update CustomJS.js
    Update für Passwort auf Profil bearbeiten Seite

--- LD-SPA/BootstrapPages/js/CustomJS.js
+++ LD-SPA/BootstrapPages/js/CustomJS.js
@@ -23,6 +23,21 @@ function showPassword2L() {
 				}
 }
 
+function showPassword2EP() {
+		var x = document.getElementById("Password-current");
+		if (x.type === "password") {
+			x.type = "text";
+			} else {
+				x.type = "password";
+				}
+	var y = document.getElementById("Password-New");
+		if (y.type === "password") {
+			y.type = "text";
+			} else {
+				y.type = "password";
+				}
+}
+
 //####################
 //Password Register validation
 function validatePassword(){
@@ -85,8 +100,68 @@ myInput.onkeyup = function() {
 }
 }
 //####################
+//Password Register validation
+function validatePassword2(){
+var myInput2 = document.getElementById("Password-New");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
 
+// When the user clicks on the password field, show the message box
+myInput2.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput2.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput2.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput2.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid-psw");
+    letter.classList.add("valid-psw");
+  } else {
+    letter.classList.remove("valid-psw");
+    letter.classList.add("invalid-psw");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput2.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid-psw");
+    capital.classList.add("valid-psw");
+  } else {
+    capital.classList.remove("valid-psw");
+    capital.classList.add("invalid-psw");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput2.value.match(numbers)) {
+    number.classList.remove("invalid-psw");
+    number.classList.add("valid-psw");
+  } else {
+    number.classList.remove("valid-psw");
+    number.classList.add("invalid-psw");
+  }
+
+  // Validate length
+  if(myInput2.value.length >= 8) {
+    length.classList.remove("invalid-psw");
+    length.classList.add("valid-psw");
+  } else {
+    length.classList.remove("valid-psw");
+    length.classList.add("invalid-psw");
+  }
+}
+}
 //####################
+
 //Enable drag and drop for Kanban-Board 
 
 	const drag = (event) => {
